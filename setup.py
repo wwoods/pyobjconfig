f39b1041eb76aeca72c41e3043e9bcc0e492a00b
wwoods <waltw@galois.com>
2020-10-14 10:56:40 -0700

    0.1.1 - fix for requirements.txt in setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,9 @@ import setuptools
 with open('README.md') as f:
     long_desc = f.read()
 
-with open('requirements.txt') as f:
-    reqs = f.read().splitlines()
-
 setuptools.setup(
         name='pyobjconfig',
-        version='0.1.0',
+        version='0.1.1',
         author='Walt Woods',
         author_email='woodswalben@gmail.com',
         description="An argparse+pydantic-based configuration system for Python.",
@@ -17,6 +14,6 @@ setuptools.setup(
         long_description_content_type='text/markdown',
         url='https://github.com/wwoods/pyobjconfig',
         packages=setuptools.find_packages(),
-        install_requires=reqs,
+        install_requires=['pydantic'],
 )
 
